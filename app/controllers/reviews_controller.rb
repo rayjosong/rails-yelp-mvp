d966ac3756dda77be027b1a65749e0901242c5c6
raymond ong <rayjosong@gmail.com>
2022-05-26 14:00:33 +0800

    done with c & r for resto

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,18 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.save
+
+    redirect_to review_path(@review)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Database cleaned.'
+
+puts 'Creating restaurants...'
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+20.times do
+  restaurant = Restaurant.new({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES.sample
+  })
+
+  restaurant.save!
+  puts "Created #{restaurant.name}"
+end
+
+puts "Seeding completed!"
